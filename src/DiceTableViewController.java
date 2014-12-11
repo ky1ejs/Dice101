8e14a3f7c238431f2ee842faee4e34c4b4ebb565
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2014-12-11 17:48:46 +0000

    Managed to get the dice to show in a label. Need to play around with resizing it next

--- src/DiceTableViewController.java
+++ src/DiceTableViewController.java
@@ -5,10 +5,12 @@ import javax.swing.*;
  */
 public class DiceTableViewController {
     private JPanel view;
+    private JLabel dice1;
 
     public DiceTableViewController() {
         view.setSize(300, 300);
-        view.setVisible(true);
+        ImageIcon image = new ImageIcon("Assets/Dice.png");
+        dice1.setIcon(image);
     }
 
     public JPanel getPanel1() {
