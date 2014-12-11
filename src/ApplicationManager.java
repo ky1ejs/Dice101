6146e8f21d66d8bf12497ce07e63898fe33cbca0
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2014-12-11 16:09:12 +0000

    ApplicationManager Class created

---
+++ src/ApplicationManager.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/**
+ * Created by kylejm on 11/12/14.
+ */
+public class ApplicationManager {
+
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame("Dice101");
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setSize(600, 600);
+        DiceTableViewController diceView = new DiceTableViewController();
+        mainFrame.add(diceView.getPanel1());
+        mainFrame.setVisible(true);
+    }
+
+}
