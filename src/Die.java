40a6bc91950004cb99c897bef3ffb9d9f3b6d51f
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2014-12-31 13:22:33 +0000

    Die class can now find it's own image. Method name in DieIntf corrected for clarity.

--- src/Die.java
+++ src/Die.java
@@ -1,5 +1,44 @@
+import javax.swing.*;
+
 /**
  * Created by kylejm on 31/12/14.
+ * //This class should be called DieFace, but the coursework spec specifies that it must be called Die
  */
-public class Die {
+public class Die implements DieIntf, Comparable<Die> {
+    ImageIcon image;
+    int value;
+
+    @Override
+    public ImageIcon getDieImage() {
+        return null;
+    }
+
+    @Override
+    public void setImage(ImageIcon image) {
+        if (value > 0 && value < 7) this.image = new ImageIcon(String.format("Assets/dice-%f", value));
+    }
+
+    @Override
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public void setValue(int value) {
+        if (value > 0 && value < 7) {
+            this.value = value;
+            setImage(null);
+        }
+    }
+
+    @Override
+    public int compareTo(Die o) {
+        if (o.getValue() < value) {
+            return -1;
+        } else if (o.getValue() > value) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
--- src/DieIntf.java
+++ src/DieIntf.java
@@ -4,7 +4,7 @@ import javax.swing.*;
  * Created by kylejm on 30/12/14.
  */
 public interface DieIntf {
-    public ImageIcon getImage();
+    public ImageIcon getDieImage();
     public void setImage(ImageIcon image);
     public int getValue();
     public void setValue(int value);
