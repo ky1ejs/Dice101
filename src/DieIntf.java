73072ac1c20bd192dd066e09b5282b429f9daa01
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2014-12-31 00:26:05 +0000

    Methods specified in coursework spec for DieIntf added

--- src/DieIntf.java
+++ src/DieIntf.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
+
 /**
  * Created by kylejm on 30/12/14.
  */
 public interface DieIntf {
+    public ImageIcon getImage();
+    public void setImage(ImageIcon image);
+    public int getValue();
+    public void setValue(int value);
 }
