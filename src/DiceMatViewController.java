bd836870443053663601cd8dd13084da27d4abdc
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-01-01 16:08:17 +0000

    WIP: DiceMatVC now displays a "Throw" button which displays the users rolled dice when pressed. There's an issue with the way the ImageLabels are layed out.

--- src/DiceMatViewController.java
+++ src/DiceMatViewController.java
@@ -1,16 +1,39 @@
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 /**
  * Created by kylejm on 11/12/14.
  */
-public class DiceMatViewController {
+public class DiceMatViewController extends MouseAdapter {
     private JPanel view;
+    private JButton throwButton;
+    private Dice dice;
+    private ArrayList<ImageLabel> userImageLabels;
+
 
     public DiceMatViewController() {
+        throwButton.addMouseListener(this);
+        dice = new Dice();
         view.setSize(300, 300);
-        ImageIcon imageIcon = new ImageIcon("Assets/Dice.png");
-        ImageLabel imageLabel = new ImageLabel(imageIcon.getImage(), 100, 100);
-        view.add(imageLabel);
+        userImageLabels = new ArrayList<ImageLabel>();
+        for (int i = 0; i < 5; i++) {
+            ImageLabel label = new ImageLabel(300, 300);
+            view.add(label);
+            userImageLabels.add(label);
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throwButton.setEnabled(false);
+        ArrayList<Die> results = dice.roll();
+        for (int i = 0; i < 5; i++) {
+            Die face = results.get(i);
+            ImageLabel label = userImageLabels.get(i);
+            label.setImage(face.getDieImage().getImage());
+        }
     }
 
     public JPanel getView() {
