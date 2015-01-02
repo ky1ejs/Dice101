0a8f853ada6b5c9085e64ea741b3ceda6dadcceb
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-01-02 19:00:07 +0000

    Removed DiceMatVC form file. Going to create view and layout in code as I feel it's easier to learn.

--- src/DiceMatViewController.java
+++ src/DiceMatViewController.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -7,22 +8,32 @@ import java.util.ArrayList;
  * Created by kylejm on 11/12/14.
  */
 public class DiceMatViewController extends MouseAdapter {
-    private JPanel view;
-    private JButton throwButton;
-    private Dice dice;
-    private ArrayList<ImageLabel> userImageLabels;
+    private JPanel view = new JPanel(new BorderLayout());
+    private JButton throwButton = new JButton("Throw");
+    private Dice dice = new Dice();
+    private ArrayList<ImageLabel> userImageLabels = new ArrayList<ImageLabel>();
+    private ArrayList<ImageLabel> computerImageLabels = new ArrayList<ImageLabel>();
 
 
     public DiceMatViewController() {
+        GridLayout diceLayout = new GridLayout(3, 2);
+        JPanel userDice = new JPanel(diceLayout);
+        JPanel computerDice = new JPanel(diceLayout);
         throwButton.addMouseListener(this);
-        dice = new Dice();
         view.setSize(300, 300);
-        userImageLabels = new ArrayList<ImageLabel>();
         for (int i = 0; i < 5; i++) {
-            ImageLabel label = new ImageLabel(300, 300);
-            view.add(label);
+            ImageLabel label = new ImageLabel(dice.getBlankFace().getImage(), 200, 200);
+            userDice.add(label);
             userImageLabels.add(label);
         }
+        for (int i = 0; i < 5; i++) {
+            ImageLabel label = new ImageLabel(dice.getBlankFace().getImage(), 200, 200);
+            computerDice.add(label);
+            computerImageLabels.add(label);
+        }
+        view.add(userDice, BorderLayout.LINE_START);
+        view.add(computerDice, BorderLayout.LINE_END);
+        view.add(throwButton, BorderLayout.PAGE_END);
     }
 
     @Override
