504d11ef8b322941c2d6473a5d00c02f1c3b12fd
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-01-02 20:24:11 +0000

    Comment added to DieIntf to explain poor method naming of getDieImage()

--- src/DieIntf.java
+++ src/DieIntf.java
@@ -4,7 +4,7 @@ import javax.swing.*;
  * Created by kylejm on 30/12/14.
  */
 public interface DieIntf {
-    public ImageIcon getDieImage();
+    public ImageIcon getDieImage(); //This should be getDieImageIcon, but the coursework spec specifies that it should be named this
     public void setImage(ImageIcon image);
     public int getValue();
     public void setValue(int value);
