62bf9385c63e8271e74e9334fa1c11ec8f8c58e6
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-01-03 17:56:28 +0000

    WIP: Significantly changed the init process of the views. Provides better positioning for stat labels. Still need to better align the labels though.

--- src/DiceMatViewController.java
+++ src/DiceMatViewController.java
@@ -11,35 +11,58 @@ public class DiceMatViewController extends MouseAdapter {
     private JPanel view = new JPanel(new BorderLayout());
     private JButton throwButton = new JButton("Throw");
     private Dice dice = new Dice();
+    private int gameStatRows = 2;
+    private int gameStatCols = 3;
+    private JPanel[][] userStatContainers = new JPanel[gameStatRows][gameStatCols];
+    private JPanel[][] computerStatContainers = new JPanel[gameStatRows][gameStatCols];
     private ArrayList<ImageLabel> userImageLabels = new ArrayList<ImageLabel>();
     private ArrayList<ImageLabel> computerImageLabels = new ArrayList<ImageLabel>();
 
 
     public DiceMatViewController() {
+        initGUI();
+    }
+
+    private void initGUI() {
+        initGameStatGUI();
+        initDiceGUI();
+        throwButton.addMouseListener(this);
+        view.add(throwButton, BorderLayout.PAGE_END);
+    }
+
+    private void initGameStatGUI() {
+        JPanel userStatsGrid = new JPanel(new GridLayout(gameStatRows, gameStatCols));
+        JPanel computerStatsGrid = new JPanel(new GridLayout(gameStatRows, gameStatCols));
+        for (int row = 0; row < gameStatRows; row++) {
+            for (int col = 0; col < gameStatCols; col++) {
+                userStatContainers[row][col] = new JPanel();
+                userStatsGrid.add(userStatContainers[row][col]);
+                computerStatContainers[row][col] = new JPanel();
+                computerStatsGrid.add(computerStatContainers[row][col]);
+            }
+        }
+        userStatContainers[1][1].add(new JLabel("User")); //TODO: Make labels show centred over each set of dice
+        computerStatContainers[1][1].add(new JLabel("Computer"));
+        JPanel statGridContainer = new JPanel(new BorderLayout());
+        statGridContainer.add(userStatsGrid, BorderLayout.LINE_START);
+        statGridContainer.add(computerStatsGrid, BorderLayout.LINE_END);
+        view.add(statGridContainer, BorderLayout.PAGE_START);
+    }
+
+    private void initDiceGUI() {
         GridLayout diceLayout = new GridLayout(3, 2);
         JPanel userDice = new JPanel(diceLayout);
         JPanel computerDice = new JPanel(diceLayout);
-        throwButton.addMouseListener(this);
-        view.setSize(300, 300);
         for (int i = 0; i < 5; i++) {
             ImageLabel label = new ImageLabel(dice.getBlankFace().getImage(), 100, 100);
             userDice.add(label);
             userImageLabels.add(label);
-        }
-        for (int i = 0; i < 5; i++) {
-            ImageLabel label = new ImageLabel(dice.getBlankFace().getImage(), 100, 100);
+            label = new ImageLabel(dice.getBlankFace().getImage(), 100, 100);
             computerDice.add(label);
             computerImageLabels.add(label);
         }
-        JLabel userLabel = new JLabel("User"); //TODO: Make labels show centred over each set of dice
-        JLabel computerLabel = new JLabel("Computer");
-        JPanel labelPanel = new JPanel(new BorderLayout());
-        labelPanel.add(userLabel, BorderLayout.LINE_START);
-        labelPanel.add(computerLabel, BorderLayout.LINE_END);
-        view.add(labelPanel, BorderLayout.PAGE_START);
         view.add(userDice, BorderLayout.LINE_START);
         view.add(computerDice, BorderLayout.LINE_END);
-        view.add(throwButton, BorderLayout.PAGE_END);
     }
 
     @Override
