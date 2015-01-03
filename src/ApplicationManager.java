4408db3f09afb3b3ad36b8f2963bd6384500cf37
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-01-03 18:25:59 +0000

    App window (JFrame) set to match min size

--- src/ApplicationManager.java
+++ src/ApplicationManager.java
@@ -9,7 +9,7 @@ public class ApplicationManager {
     public static void main(String[] args) {
         JFrame mainFrame = new JFrame("Dice101");
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        mainFrame.setSize(600, 600);
+        mainFrame.setSize(450, 500);
         mainFrame.setMinimumSize(new Dimension(450, 500));
         DiceMatViewController diceView = new DiceMatViewController();
         mainFrame.add(diceView.getView());
